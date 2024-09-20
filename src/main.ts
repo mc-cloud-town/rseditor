37930c0d920dfdb436c5abc6dd9c35188ef4bd2e
a3510377 <a102009102009@gmail.com>
2024-09-20 14:37:44 +0000

    Refactor Controls.ts and add crafter

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,70 @@
 import './style.css';
 import p5 from 'p5';
+import Controls from './utils/Controls';
+import { BLOCK_NAMES } from './utils/Images';
+
+const IMAGE_MAP = new Map();
 
 const canvas = new p5((p: p5) => {
+  let firstIsDraw = false;
+  let nowPressMoveBuffers: { [key: string]: boolean } = {};
+  const gridContents: {
+    [x: number]: { [y: number]: string | undefined } | undefined;
+  } = {};
+
+  BLOCK_NAMES.forEach((e) => IMAGE_MAP.set(e, p.loadImage(`assets/${e}.webp`)));
+
   p.setup = () => {
     p.createCanvas(400, 400);
+  };
+
+  const nowPos = () => {
+    const x = Math.floor(p.mouseX / 30);
+    const y = Math.floor(p.mouseY / 30);
+
+    return { x, y, str: `${x}-${y}` };
+  };
+
+  // return false: erase, true: draw
+  const setGridImage = (pos?: { x: number; y: number }, clear?: boolean) => {
+    const { x, y } = pos ?? nowPos();
+
+    if (clear === true) {
+      if (gridContents[x]?.[y] !== undefined) {
+        delete gridContents[x][y];
+      }
+
+      canvas.erase();
+      canvas.rect(x * 30, y * 30, 30, 30);
+      canvas.noErase();
+      return false;
+    }
+
+    gridContents[x] ??= {};
+    gridContents[x][y] = 'x';
+
+    canvas.image(IMAGE_MAP.get('arail'), x * 30, y * 30, 30, 30);
+    return true;
+  };
+
+  p.mousePressed = () => {
+    firstIsDraw = setGridImage();
+  };
+
+  p.mouseDragged = () => {
+    const pos = nowPos();
+    if (nowPressMoveBuffers[pos.str]) return;
+
+    setGridImage(pos, !firstIsDraw);
+
+    nowPressMoveBuffers[pos.str] = true;
+  };
+
+  p.mouseReleased = () => {
+    nowPressMoveBuffers = {};
   };
 }, document.getElementById('app')!);
 
 // canvas.mouseWheel(e => p5.C)
+const canvasControls = new Controls(canvas);
+// canvasControls.worldZoom({ x: 200, y: 200, deltaY: -1, factor: 0.05 })
---
+++ src/utils/Images.ts
@@ -0,0 +1,171 @@
+export const BLOCK_NAMES = [
+  //arail
+  'arail',
+  'arailsl',
+  'arailsr',
+
+  //barrel
+  'barrel',
+
+  //block
+  'air',
+  'glasst',
+  'glassw',
+  'glazedterracotta',
+  'honeyblock',
+  'ironblock',
+  'noteblock',
+  'obsidian',
+  'rsblock',
+  'sand',
+  'scaffolding',
+  'slabt',
+  'slimeblock',
+  'smoothstone',
+  'soulsand',
+  'unknown',
+
+  //cake
+  'cake14',
+
+  //cauldron
+  'cauldron',
+
+  //chest
+  'chest',
+  'chestdl',
+  'chestdr',
+
+  //comparator
+  'comparatorloff',
+  'comparatorlon',
+  'comparatorroff',
+  'comparatorron',
+
+  //target
+  'target',
+
+  //composter
+  'composter',
+
+  //door
+
+  //dropper
+  'dropperd',
+  'dropperl',
+  'dropperr',
+  'dropperu',
+
+  //dust
+  'dustoff',
+  'duston',
+
+  //fencegate
+  'fencegatec',
+  'fencegateo',
+
+  //hopper
+  'hopperb',
+  'hopperd',
+  'hopperl',
+  'hopperr',
+
+  //lamp
+  'lampoff',
+  'lampon',
+
+  //observer
+  'observerb',
+  'observerd',
+  'observerl',
+  'observerr',
+  'observeru',
+
+  //piston
+  'pistonb',
+  'pistond',
+  'pistonf',
+  'pistonl',
+  'pistonr',
+  'pistonu',
+  'stickypistonb',
+  'stickypistond',
+  'stickypistonf',
+  'stickypistonl',
+  'stickypistonr',
+  'stickypistonu',
+  'pistonheadd',
+  'pistonheadl',
+  'pistonheadr',
+  'pistonheadu',
+  'stickypistonheadd',
+  'stickypistonheadl',
+  'stickypistonheadr',
+  'stickypistonheadu',
+  'pistonbodyd',
+  'pistonbodyl',
+  'pistonbodyr',
+  'pistonbodyu',
+
+  //prail
+  'prail',
+  'prailsl',
+  'prailsr',
+
+  //repeater
+  'repeaterloff',
+  'repeaterlon',
+  'repeaterroff',
+  'repeaterron',
+  'repeaterfoff',
+  'repeaterfon',
+
+  //shulkerbox
+  'shulkerboxd',
+  'shulkerboxl',
+  'shulkerboxr',
+  'shulkerboxu',
+
+  //torch
+  'torchoff',
+  'torchon',
+  'torchloff',
+  'torchlon',
+  'torchroff',
+  'torchron',
+
+  //trapdoor
+  'trapdoord',
+  'trapdoorl',
+  'trapdooru',
+  'trapdoorr',
+
+  //wooden trapdoor
+  'wtrapdoord',
+  'wtrapdoorl',
+  'wtrapdooru',
+  'wtrapdoorr',
+
+  //stair
+  'stairdl',
+  'stairul',
+  'stairur',
+  'stairdr',
+
+  //based
+  'powderedsnow',
+
+  //water
+  'water',
+
+  //lava
+  'lava',
+
+  //ice
+  'ice',
+
+  //crafter
+  'crafter',
+] as const;
+
+export type TBlockName = (typeof BLOCK_NAMES)[number];
