962171fbbbd1b1d7dbd3aceeb051b95473d52898
a3510377 <a102009102009@gmail.com>
2024-09-21 03:28:27 +0000

    Refactor main.ts and index.html to add save and copy buttons

--- src/main.ts
+++ src/main.ts
@@ -5,66 +5,79 @@ import { BLOCK_NAMES } from './utils/Images';
 
 const IMAGE_MAP = new Map();
 
-const canvas = new p5((p: p5) => {
-  let firstIsDraw = false;
-  let nowPressMoveBuffers: { [key: string]: boolean } = {};
-  const gridContents: {
-    [x: number]: { [y: number]: string | undefined } | undefined;
-  } = {};
+const canvas = new p5((p5) => {
+  p5.setup = () => {
+    let firstIsDraw = false;
+    let nowPressMoveBuffers: { [key: string]: boolean } = {};
+    const gridContents: {
+      [x: number]: { [y: number]: string | undefined } | undefined;
+    } = {};
+    const canvas = p5.createCanvas(400, 400);
 
-  BLOCK_NAMES.forEach((e) => IMAGE_MAP.set(e, p.loadImage(`assets/${e}.webp`)));
+    BLOCK_NAMES.forEach((e) => {
+      IMAGE_MAP.set(e, p5.loadImage(`assets/${e}.webp`));
+    });
 
-  p.setup = () => {
-    p.createCanvas(400, 400);
-  };
+    const getPos = () => {
+      const x = Math.floor(p5.mouseX / 30);
+      const y = Math.floor(p5.mouseY / 30);
 
-  const getPos = () => {
-    const x = Math.floor(p.mouseX / 30);
-    const y = Math.floor(p.mouseY / 30);
+      return { x, y, str: `${x}-${y}` };
+    };
 
-    return { x, y, str: `${x}-${y}` };
-  };
+    // return false: erase, true: draw
+    const setGridImage = (pos?: { x: number; y: number }, clear?: boolean) => {
+      const { x, y } = pos ?? getPos();
 
-  // return false: erase, true: draw
-  const setGridImage = (pos?: { x: number; y: number }, clear?: boolean) => {
-    const { x, y } = pos ?? getPos();
+      if (clear || gridContents[x]?.[y]) {
+        if (gridContents[x]?.[y]) {
+          delete gridContents[x][y];
+        }
 
-    if (clear || gridContents[x]?.[y]) {
-      if (gridContents[x]?.[y]) {
-        delete gridContents[x][y];
+        p5.erase();
+        p5.rect(x * 30, y * 30, 30, 30);
+        p5.noErase();
+        return false;
       }
 
-      canvas.erase();
-      canvas.rect(x * 30, y * 30, 30, 30);
-      canvas.noErase();
-      return false;
-    }
+      gridContents[x] ??= {};
+      gridContents[x][y] = 'x';
 
-    gridContents[x] ??= {};
-    gridContents[x][y] = 'x';
+      p5.image(IMAGE_MAP.get('arail'), x * 30, y * 30, 30, 30);
+      return true;
+    };
 
-    canvas.image(IMAGE_MAP.get('arail'), x * 30, y * 30, 30, 30);
-    return true;
-  };
+    p5.mousePressed = ({ button }: MouseEvent) => {
+      if (button !== 0) return; // 0 is left click
 
-  p.mousePressed = () => {
-    firstIsDraw = setGridImage();
-  };
+      firstIsDraw = setGridImage();
+    };
 
-  p.mouseDragged = () => {
-    const pos = getPos();
-    if (nowPressMoveBuffers[pos.str]) return;
+    p5.mouseDragged = ({ button }: MouseEvent) => {
+      if (button !== 0) return; // 0 is left click
 
-    setGridImage(pos, !firstIsDraw);
+      const pos = getPos();
+      if (nowPressMoveBuffers[pos.str]) return;
 
-    nowPressMoveBuffers[pos.str] = true;
-  };
+      setGridImage(pos, !firstIsDraw);
 
-  p.mouseReleased = () => {
-    nowPressMoveBuffers = {};
+      nowPressMoveBuffers[pos.str] = true;
+    };
+
+    p5.mouseReleased = () => {
+      nowPressMoveBuffers = {};
+    };
+
+    document.getElementById('save')!.addEventListener('click', () => {
+      p5.save('grid.png');
+    });
+
+    document.getElementById('copy')!.addEventListener('click', () => {
+      canvas.elt.toBlob((blob: Blob) => {
+        const item = new ClipboardItem({ 'image/png': blob });
+
+        navigator.clipboard.write([item]);
+      });
+    });
   };
 }, document.getElementById('app')!);
-
-// canvas.mouseWheel(e => p5.C)
-const canvasControls = new Controls(canvas);
-// canvasControls.worldZoom({ x: 200, y: 200, deltaY: -1, factor: 0.05 })
